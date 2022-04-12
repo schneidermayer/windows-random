1045692dd13d0cd7f7317873a5205a3d93226fb3
Hannes Schneidermayer (e544416) <hannes.schneidermayer@sbb.ch>
2022-04-12 20:27:31 +0200

    shortcuts for switching theme

---
+++ set light.bat
@@ -0,0 +1,2 @@
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d "1" /f
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d "1" /f
